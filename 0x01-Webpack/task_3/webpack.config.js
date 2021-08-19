ce88c3c6fa1d9061334a6d9111892c19c421c633
Diego Monroy <difemo91@gmail.com>
2021-08-19 02:09:09 +0000

    [FIX - WEBPACK] add correct content base to devServer

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     },
   },
   devServer: {
-    contentBase: path.join(__dirname, '/public'),
+    contentBase: path.join(__dirname, './public'),
     compress: true,
     port: 8564,
   },
