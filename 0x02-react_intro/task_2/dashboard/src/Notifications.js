15b9a41872b1fa15ec6f259b0a157beea3e574ba
Diego Monroy <difemo91@gmail.com>
2021-11-11 13:40:13 -0500

    [FEAT - REACT] add close button

--- 0x02-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Notifications.css';
+import closeIcon from './close-icon.png';
 
 function Notifications() {
   return (
     <>
       <div className="Notifications">
         <p>Here is the list of notifications</p>
+        <button style={{
+          float: 'right',
+          marginTop: '-70px',
+        }} aria-label='Close'>
+          <img src={closeIcon} alt='close' />
+        </button>
       </div>
     </>
   );
